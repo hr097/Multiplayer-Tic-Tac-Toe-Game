afde9709f55430b0eace18113b351457fb6e1a52
Harshil Ramani <harshilramani9777@gmail.com>
2022-02-15 01:36:46 +0530

    Code added for editing and understanding purpose

---
+++ SOURCE CODE/Rules.Designer.vb
@@ -0,0 +1,283 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FormRules
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormRules))
+        Me.LabelRulesHeading = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.PictureBoxdeveloper = New System.Windows.Forms.PictureBox()
+        Me.ToolTipDeveloper = New System.Windows.Forms.ToolTip(Me.components)
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        CType(Me.PictureBoxdeveloper, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'LabelRulesHeading
+        '
+        Me.LabelRulesHeading.AutoSize = True
+        Me.LabelRulesHeading.Font = New System.Drawing.Font("SF Compact Display", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.LabelRulesHeading.ForeColor = System.Drawing.Color.Navy
+        Me.LabelRulesHeading.Location = New System.Drawing.Point(86, 32)
+        Me.LabelRulesHeading.Name = "LabelRulesHeading"
+        Me.LabelRulesHeading.Size = New System.Drawing.Size(462, 56)
+        Me.LabelRulesHeading.TabIndex = 0
+        Me.LabelRulesHeading.Text = "PLAYERS MUST FOLLOW ALL BELOW  RULES " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "IN  ORDER TO PLAY : -"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("SF Compact Display", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(39, 153)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(487, 24)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "→Each player will get chance in sequential manner."
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("SF Compact Display", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(39, 233)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(582, 24)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "→Quiting game in between  will result into loosing game data."
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("SF Compact Display", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label4.Location = New System.Drawing.Point(39, 270)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(256, 24)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "→Eventually There will be  "
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("SF Compact Display", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label5.Location = New System.Drawing.Point(39, 118)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(492, 24)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "→Each player will have different signature in game. "
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("SF Compact Display", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label6.Location = New System.Drawing.Point(39, 192)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(527, 24)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "→No single player can have multiple sequential chance."
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("SF Compact Display", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label7.Location = New System.Drawing.Point(39, 304)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(192, 24)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = "→You have to make "
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.Button1.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.Button1.FlatAppearance.BorderSize = 5
+        Me.Button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gainsboro
+        Me.Button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gainsboro
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button1.Font = New System.Drawing.Font("SF Compact Display", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.Location = New System.Drawing.Point(208, 414)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(190, 37)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "OKAY , WE AGREE"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("SF Compact Display", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label8.ForeColor = System.Drawing.Color.Green
+        Me.Label8.Location = New System.Drawing.Point(292, 270)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(117, 24)
+        Me.Label8.TabIndex = 9
+        Me.Label8.Text = "one winner "
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("SF Compact Display", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label9.Location = New System.Drawing.Point(401, 270)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(21, 24)
+        Me.Label9.TabIndex = 10
+        Me.Label9.Text = " /"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("SF Compact Display", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label10.ForeColor = System.Drawing.Color.Navy
+        Me.Label10.Location = New System.Drawing.Point(428, 270)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(154, 24)
+        Me.Label10.TabIndex = 11
+        Me.Label10.Text = "tie in the game."
+        '
+        'PictureBoxdeveloper
+        '
+        Me.PictureBoxdeveloper.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.PictureBoxdeveloper.BackgroundImage = CType(resources.GetObject("PictureBoxdeveloper.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBoxdeveloper.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.PictureBoxdeveloper.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBoxdeveloper.Location = New System.Drawing.Point(533, 389)
+        Me.PictureBoxdeveloper.Name = "PictureBoxdeveloper"
+        Me.PictureBoxdeveloper.Size = New System.Drawing.Size(65, 62)
+        Me.PictureBoxdeveloper.TabIndex = 12
+        Me.PictureBoxdeveloper.TabStop = False
+        Me.ToolTipDeveloper.SetToolTip(Me.PictureBoxdeveloper, "Developed by Harshil Ramani " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Github ID : https://github.com/hr097")
+        '
+        'ToolTipDeveloper
+        '
+        Me.ToolTipDeveloper.AutomaticDelay = 10
+        Me.ToolTipDeveloper.AutoPopDelay = 10000
+        Me.ToolTipDeveloper.InitialDelay = 10
+        Me.ToolTipDeveloper.IsBalloon = True
+        Me.ToolTipDeveloper.ReshowDelay = 2
+        Me.ToolTipDeveloper.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info
+        Me.ToolTipDeveloper.ToolTipTitle = "Developer's credit"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("SF Compact Display", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.ForeColor = System.Drawing.Color.Navy
+        Me.Label2.Location = New System.Drawing.Point(12, 17)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(68, 71)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "»"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("SF Compact Display", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label11.Location = New System.Drawing.Point(39, 364)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(359, 24)
+        Me.Label11.TabIndex = 14
+        Me.Label11.Text = "→Play cheerfully and enjoy the game."
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("SF Compact Display", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label12.ForeColor = System.Drawing.Color.Green
+        Me.Label12.Location = New System.Drawing.Point(228, 304)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(319, 24)
+        Me.Label12.TabIndex = 15
+        Me.Label12.Text = "3 continuous signature sequence"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Font = New System.Drawing.Font("SF Compact Display", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label13.Location = New System.Drawing.Point(39, 328)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(159, 24)
+        Me.Label13.TabIndex = 16
+        Me.Label13.Text = "    in order to win"
+        '
+        'FormRules
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.ClientSize = New System.Drawing.Size(632, 473)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.PictureBoxdeveloper)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.LabelRulesHeading)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(650, 520)
+        Me.MinimumSize = New System.Drawing.Size(650, 520)
+        Me.Name = "FormRules"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Rules & Conditions"
+        CType(Me.PictureBoxdeveloper, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LabelRulesHeading As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents PictureBoxdeveloper As PictureBox
+    Friend WithEvents ToolTipDeveloper As ToolTip
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+End Class
---
+++ SOURCE CODE/Tic-tae-toe.vb
@@ -0,0 +1,30 @@
+﻿
+
+
+Public Class TictaetoeForm
+
+    Private Sub TictaetoeForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBoxNumOfPlayers.SelectedIndex = 0
+    End Sub
+
+    Private Sub LinkLabelSeeRules_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabelSeeRules.LinkClicked
+
+        Dim RulesForm As New FormRules
+
+        RulesForm.Show()
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim GameDisplayForm As New FormGameDisplay
+        'If GameDisplayForm Is Nothing Then
+        GameDisplayForm = New FormGameDisplay
+        GameDisplayForm.myCaller = Me
+        'End If
+        GameDisplayForm.Show()
+    End Sub
+End Class
+
+
+
+
